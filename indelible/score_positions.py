a0d34d845431a9568713f3aa6bd2154134dc9e2e
Eugene Gardner <egardner@umaryland.edu>
2019-08-19 16:54:25 +0100

    Investigating slowdown in score module

--- indelible/score_positions.py
+++ indelible/score_positions.py
@@ -25,7 +25,7 @@ import bz2
 
 def trainForest(training_file_path):
     df = pd.read_csv(training_file_path, sep="\t")
-    values = df.ix[:, df.columns - ["chrom", "position", "annotation", "sample", "seq_longest"]]
+    values = df.ix[:, df.columns - ["chrom", "position", "annotation", "sample", "seq_longest", "pct_double_split"]]
     outcome = df["annotation"]
     clf = RandomForestClassifier(n_estimators=10000)
     clf.fit(values, outcome)
@@ -37,10 +37,12 @@ def score_file(forest_path, testing_file_path):
     df = pd.read_csv(testing_file_path, sep="\t")
     values = df.loc[:, df.columns.difference(["chrom", "position", "seq_longest", "pct_double_split"])]
     clf = loadForest(forest_path)
-    predicted_class = clf.predict(values)
-    df["predicted"] = predicted_class
-    df["prob_N"] = clf.predict_proba(values).T[0]
-    df["prob_Y"] = clf.predict_proba(values).T[1]
+
+    # for line in values:
+    #     predicted_class = clf.predict(line)
+    # df["predicted"] = predicted_class
+    # df["prob_N"] = clf.predict_proba(values).T[0]
+    # df["prob_Y"] = clf.predict_proba(values).T[1]
     return df
 
 
