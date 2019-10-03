5334ef9669b1eef24d1285d4efee2ff58e10e141
Eugene Gardner <egardner@umaryland.edu>
2019-10-03 10:41:20 +0100

    Reannotating

--- indelible/annotate.py
+++ indelible/annotate.py
@@ -210,23 +210,22 @@ def annotate(input_path, output_path, database, config):
     scored_file = csv.DictReader(open(input_path), delimiter="\t")
     ensembl_exons = bedtools.BedTool(config['ensembl_exons'])
     db = read_database(database)
-
     # Prepare outputfile
-    new_fieldnames = scored_file.fieldnames
-    new_fieldnames.extend(("ddg2p", 'hgnc', 'hgnc_constrained', "exonic", "transcripts", "maf",
-                           "blast_hit", "blast_strand", "blast_identity", "blast_dist", "blast_hgnc"))
-    output_file = csv.DictWriter(open(output_path, 'w'), fieldnames=new_fieldnames, delimiter="\t", lineterminator="\n")
+    # new_fieldnames = scored_file.fieldnames
+    # new_fieldnames.extend(("ddg2p", 'hgnc', 'hgnc_constrained', "exonic", "transcripts", "maf",
+    #                        "blast_hit", "blast_strand", "blast_identity", "blast_dist", "blast_hgnc"))
+    # output_file = csv.DictWriter(open(output_path, 'w'), fieldnames=new_fieldnames, delimiter="\t", lineterminator="\n")
     output_file = csv.DictWriter(open(output_path, 'w'), fieldnames=scored_file.fieldnames, delimiter="\t", lineterminator="\n")
     output_file.writeheader()
     # Prepare searchable hashes
-    bhash = create_blast_hash(input_path)
+    # bhash = create_blast_hash(input_path)
     constraint_hash = create_exac_constraint_hash(config)
     ddg2p_db = read_ddg2p(config['ddg2p_bed'])
     hgnc_db = read_hgnc_genes(config['hgnc_file'])
 
     for v in scored_file:
 
-        v = annotate_blast(v, bhash, ddg2p_db, constraint_hash, hgnc_db)
+        # v = annotate_blast(v, bhash, ddg2p_db, constraint_hash, hgnc_db)
 
         chrom = v["chrom"]
         pos = int(v["position"])
